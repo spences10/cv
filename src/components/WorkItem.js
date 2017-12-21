6f941889c16bf3c0e6fc7bc88b59dcfe4074dc0a
spences10 <spences10apps@gmail.com>
2017-12-21 15:28:11 +0000

    work item

---
+++ src/components/WorkItem.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import moment from 'moment'
+
+const WorkItem = props => {
+  const getWorkDates = () => {
+    const startdate = moment(props.workItemData.startDate).format('MMM, YYYY')
+    let enddate = null
+    if (props.workItemData.endDate !== '') {
+      enddate = moment(props.workItemData.endDate).format('MMM, YYYY')
+    } else {
+      enddate = 'Present'
+    }
+
+    return (
+      <span className="startdate">
+        {startdate} - {enddate}
+      </span>
+    )
+  }
+
+  const getHighlights = props.workItemData.highlights.map(function(
+    item,
+    index
+  ) {
+    return <li key={index}>{item}</li>
+  })
+
+  return (
+    <div className="workItem">
+      <h3>
+        {props.workItemData.position}, <span>{props.workItemData.company}</span>
+      </h3>
+      <p className="workDates">{getWorkDates()}</p>
+      <p>{props.workItemData.summary}</p>
+      <ul>{getHighlights}</ul>
+    </div>
+  )
+}
+
+export default WorkItem
