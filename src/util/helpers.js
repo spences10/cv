4696966fd363e4e98c65bce4c39f95d02e8ad5a7
spences10 <spences10apps@gmail.com>
2018-04-29 19:25:47 +0100

    light helpers remove the functions that i was trying to use to flatten out an array

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -3,7 +3,3 @@ import axios from 'axios'
 export async function fetchCvData(dataUrl) {
   return await axios.get(dataUrl)
 }
-
-export function mergeArrays(base, ...arrays) {
-  return base.concat(...arrays).sort((a, b) => a - b)
-}
