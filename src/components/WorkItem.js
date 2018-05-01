02c7542a08103dd84edfdae8836b5a510d4e6330
spences10 <spences10apps@gmail.com>
2018-05-01 11:06:08 +0100

    start styling work item

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -1,42 +1,53 @@
 import React from 'react'
 import dayjs from 'dayjs'
+import styled from 'styled-components'
+
+const WorkItemWrapper = styled.div``
+
+const WorkTitleLocation = styled.h3``
+
+const StyledWorkDates = styled.p``
+
+const WorkItemSummary = styled.p``
+
+const StyledHighlights = styled.ul``
 
 const WorkItem = props => {
   const getWorkDates = () => {
     const startDate = dayjs(props.workItemData.startDate).format(
       'MMM, YYYY'
     )
-    let endDate = null
-    if (props.workItemData.endDate !== '') {
-      endDate = dayjs(props.workItemData.endDate).format('MMM, YYYY')
-    } else {
-      endDate = 'Present'
+    const endDate = () => {
+      if (props.workItemData.endDate !== '') {
+        return dayjs(props.workItemData.endDate).format('MMM, YYYY')
+      } else {
+        return 'Present'
+      }
     }
 
     return (
       <span className="startdate">
-        {startDate} - {endDate}
+        {startDate} - {endDate()}
       </span>
     )
   }
 
-  const getHighlights = props.workItemData.highlights.map(function(
-    item,
-    index
-  ) {
-    return <li key={index}>{item}</li>
-  })
+  const getHighlights = props.workItemData.highlights.map(
+    (item, index) => {
+      return <li key={index}>{item}</li>
+    }
+  )
 
   return (
-    <div className="workItem">
-      <h3>
+    <WorkItemWrapper>
+      <WorkTitleLocation>
         {props.workItemData.position},{' '}
         <span>{props.workItemData.company}</span>
-      </h3>
-      <p className="workDates">{getWorkDates()}</p>
-      <p>{props.workItemData.summary}</p>
-      <ul>{getHighlights}</ul>
-    </div>
+      </WorkTitleLocation>
+      <StyledWorkDates>{getWorkDates()}</StyledWorkDates>
+      <WorkItemSummary>{props.workItemData.summary}</WorkItemSummary>
+      <StyledHighlights>{getHighlights}</StyledHighlights>
+    </WorkItemWrapper>
   )
 }
 
