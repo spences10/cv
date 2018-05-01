22c2b13e0bd1d7e4f55529f71b507063a6120668
spences10 <spences10apps@gmail.com>
2018-05-01 12:48:04 +0100

    set url to alias

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
       volunteer: [],
       work: []
     },
-    apiUrl: 'https://cvjson-tvjsvcrqyp.now.sh/'
+    apiUrl: 'https://cvjson.now.sh/'
   }
 
   componentDidMount() {
