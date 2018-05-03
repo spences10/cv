164a5f11e0dda9b8b86361a0a2a700244b8a06c6
spences10 <spences10apps@gmail.com>
2018-05-03 16:26:46 +0100

    remove .css and styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,8 +8,6 @@ import styled, { ThemeProvider } from 'styled-components'
 import { theme1, theme2 } from '../theme/globalStyle'
 import { siteMeta, nameContent } from '../constants'
 
-import './index.css'
-
 class Layout extends React.Component {
   state = {
     apiUrl: 'https://cvjson.now.sh/',
@@ -20,23 +18,8 @@ class Layout extends React.Component {
     return (
       <ThemeProvider theme={this.state.theme}>
         <div>
-          <Helmet
-            title={nameContent}
-            meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' }
-            ]}
-          />
-          <Header siteTitle={nameContent} />
-          <div
-            style={{
-              margin: '0 auto',
-              maxWidth: 960,
-              padding: '0px 1.0875rem 1.45rem',
-              paddingTop: 0
-            }}>
-            {children()}
-          </div>
+          <Helmet title={nameContent} meta={siteMeta} />
+          {children()}
         </div>
       </ThemeProvider>
     )
