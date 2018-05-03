283a6789d6a27a11deef586604cdb6354740f7e3
spences10 <spences10apps@gmail.com>
2018-05-03 16:16:59 +0100

    add dtate and theme provider

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,31 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
+import { theme1, theme2 } from '../theme/globalStyle'
 
 import Header from '../components/header'
 import './index.css'
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' }
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}>
-      {children()}
-    </div>
-  </div>
-)
+class Layout extends React.Component {
+  state = {
+    apiUrl: 'https://cvjson.now.sh/',
+    theme: theme1
+  }
+  render() {
+    const { children, data } = this.props
+    return (
+      <ThemeProvider theme={this.state.theme}>
+        <div>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' }
+            ]}
+          />
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0
+            }}>
+            {children()}
+          </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.func
