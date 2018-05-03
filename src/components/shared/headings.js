bab7182443149c33e89441d860336a3d632ec5b9
spences10 <spences10apps@gmail.com>
2018-05-03 06:54:27 +0100

    add heading and uppercase

--- src/components/shared/headings.js
+++ src/components/shared/headings.js
@@ -1,8 +1,17 @@
 // heading are uppercase with spacing
+
+// normal font weight 400-500
+// heavy font weight 600-700
 import styled from 'styled-components'
 
-const Heading = styled.p`
+export const Heading = styled.p`
+  color: ${props => props.theme.dark};
+  font-family: ${props => props.theme.font};
+  font-weight: 700;
+`
+
+export const UpperCaseHeading = Heading.extend`
   text-transform: uppercase;
-  font-weight: 500;
-  ${props => props.theme.font};
+  font-family: ${props => props.theme.font};
+  font-weight: 700;
 `
