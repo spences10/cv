8740b1eee37f27ffac15a8142bde033a8e763fca
spences10 <spences10apps@gmail.com>
2018-05-03 10:56:54 +0100

    moce react file ready for delete

---
+++ delete/src/components/ThemeSelect.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Select = styled.select`
+  margin: 2rem 0.5rem;
+  padding: 0rem 0.5rem;
+
+  font-family: Roboto;
+  font-size: 1rem;
+
+  border: 1px solid ${props => props.theme.light};
+  box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.1);
+  background: ${props => props.theme.light};
+  border-radius: 2px;
+`
+
+export const SelectOpt = styled.option`
+  font-family: Roboto;
+  font-size: 1rem;
+`
+
+class ThemeSelect extends React.Component {
+  render() {
+    return (
+      <div>
+        <Select onChange={e => this.props.handleThemeChange(e)}>
+          <SelectOpt value="theme1">Theme 1</SelectOpt>
+          <SelectOpt value="theme2">Theme 2</SelectOpt>
+        </Select>
+      </div>
+    )
+  }
+}
+
+export default ThemeSelect
---
+++ delete/src/components/Work.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import WorkItem from './WorkItem'
+import styled from 'styled-components'
+
+import { ItemWrapper as IW } from './shared/ItemWrapper'
+
+const WorkWrapper = IW.extend`
+  grid-area: w;
+`
+
+const Work = props => {
+  const getWorkExperience = () => {
+    const workItems = []
+    props.workData.forEach((item, index) => {
+      workItems.push(<WorkItem key={index} workItemData={item} />)
+    })
+    return workItems
+  }
+
+  return <WorkWrapper>{getWorkExperience()}</WorkWrapper>
+}
+
+export default Work
