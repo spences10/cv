1a3955f834c3c05b9721b24ad9f31e46cee4f22b
spences10 <spences10apps@gmail.com>
2018-05-04 13:15:30 +0100

    gradient top border

--- src/components/shared/ItemWrapper.js
+++ src/components/shared/ItemWrapper.js
@@ -1,13 +1,23 @@
 import styled from 'styled-components'
 
 export const ItemWrapper = styled.div`
-  border-top: 4px solid ${props => props.theme.primary};
+  /* border-top: 4px solid ${props => props.theme.primary}; */
   border-radius: 5px;
   box-shadow: 0 13px 20px rgba(0, 0, 0, 0.3);
   margin: 1rem;
-  padding: 1rem;
+  padding: 0rem 0rem 1rem 0rem;
   font-family: ${props => props.theme.fontBody};
   font-weight: 400;
   color: ${props => props.theme.dark};
   background: ${props => props.theme.foreground};
+  :before {
+    border-radius: 5px 5px 0px 0px;
+    height: 4px;
+    display: block;
+    background: linear-gradient(
+      0.25turn, 
+      ${props => props.theme.primary}, 
+      ${props => props.theme.secondary});
+    content: '';
+  }
 `
