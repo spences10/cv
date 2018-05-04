af471f70966cba1acf54a7c1187a4841fd6d0683
spences10 <spences10apps@gmail.com>
2018-05-04 05:25:17 +0100

    add sc and components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+import { Dump } from '../util/helpers'
+import About from '../components/About'
+import Work from '../components/Work'
+import Skills from '../components/Skills'
+import Education from '../components/Education'
+import ThemeSelect from '../components/ThemeSelect'
+
+const PageContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    '. . a a a a . . '
+    '. . w w w w . . '
+    '. . s s s s . . '
+    '. . e e e e . . ';
+  background: ${props => props.theme.background};
+`
+
+const IndexPage = props => {
+  const { basics, work, skills, education } = props.data.allCv.edges
+  return (
+    <PageContainer>
+      <Dump
+        props={props.data.allCv.edges}
+        pageResources={props.pageResources}
+      />
+      {/* <About aboutData={basics} /> */}
+      {/* <Work workData={work} /> */}
+      {/* <Skills skillsData={skills} /> */}
+      {/* <Education educationData={education} /> */}
+    </PageContainer>
+  )
+}
 
 export default IndexPage
 
