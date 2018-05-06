28601b9e8ccf0cbe762b0ba954af2a6172193992
spences10 <spences10apps@gmail.com>
2018-05-06 08:49:44 +0100

    remove babel-plugin-styled-components bc/ 🤷‍♀️

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: 'Gatsby Default Starter'
   },
   plugins: [
-    'babel-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
     'gatsby-plugin-styled-components'
