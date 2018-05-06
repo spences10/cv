bbf2fff531a5592e2b6783696578fa4239c871e1
spences10 <spences10apps@gmail.com>
2018-05-06 08:34:20 +0100

    change config order

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ module.exports = {
     title: 'Gatsby Default Starter'
   },
   plugins: [
+    'babel-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-next',
-    'gatsby-plugin-styled-components',
-    'babel-plugin-styled-components'
+    'gatsby-plugin-styled-components'
   ]
 }
