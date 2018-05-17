990119d7319c822d2786c2593da4af8e3b495e07
spences10 <spences10apps@gmail.com>
2018-05-17 06:51:14 +0100

    return styled list

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,73 @@
 import React from 'react'
 
-import { Dump } from '../util/helpers'
+// import { Dump } from '../util/helpers'
 
-const Dashboard = props => {
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import {
+  ItemWrapper as IW,
+  ItemHeader as IH
+} from './shared/SharedComponents'
+
+const DashboardWrapper = IW.extend`
+  grid-area: d;
+`
+
+const ContentWrapper = styled.div`
+  margin: 0.5rem 0.5rem 0.5rem 0.5rem;
+  padding: 0.5rem 0.5rem 0.5rem 0.51rem;
+`
+
+const SkillHeader = styled.span`
+  font-size: 0.8rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`
+
+const StyledList = styled.ul`
+  margin: 0.5rem 0.0125rem;
+  padding: 0.5rem 0.0125rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: auto;
+`
+
+const StyledLi = styled.li`
+  display: inline-block;
+  margin: 0.25rem 0.25rem 0.25rem 0.25rem;
+  padding: 0.25rem 0.8rem 0.25rem 0.5rem;
+  color: 1px solid ${props => props.theme.fontLight};
+  border-radius: 15px;
+  border: 1px solid ${props => props.theme.primary};
+  &:hover {
+    background: linear-gradient(
+      0.25turn,
+      ${props => props.theme.primary},
+      ${props => props.theme.secondary}
+    );
+    color: ${props => props.theme.light};
+    border: 1px solid ${props => props.theme.background};
+  }
+`
+
+const Dashboard = ({ sections }) => {
   return (
-    <React.Fragment>
-      {Object.keys(props).map(key => {
-        console.log(key)
-      })}
-      <Dump props={props} />
-    </React.Fragment>
+    <DashboardWrapper>
+      <ContentWrapper>
+        <StyledList>
+          {sections.map((section, index) => {
+            return <StyledLi key={index}>{section}</StyledLi>
+          })}
+          {/* <Dump props={sections} /> */}
+        </StyledList>
+      </ContentWrapper>
+    </DashboardWrapper>
   )
+}
+
+Dashboard.propTypes = {
+  sections: PropTypes.array
 }
 
 export default Dashboard
