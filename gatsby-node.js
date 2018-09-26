74e5abea89b6891d8dc3268a0bc74be48175fd8e
spences10 <spences10apps@gmail.com>
2018-09-26 21:33:30 +0100

    add prefix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,16 @@
 const axios = require('axios')
-const crypto = require('crypto')
-
 const createNodeHelpers = require('gatsby-node-helpers').default
 
-const { createNodeFactory } = createNodeHelpers()
+const { createNodeFactory } = createNodeHelpers({
+  typePrefix: 'cvData'
+})
 
 const CV = createNodeFactory('CV')
 
 exports.sourceNodes = async ({ actions }) => {
   const { createNode } = actions
 
-  const fetchCvData = () => axios.get('https://cvjson.now.sh/')
+  const fetchCvData = () => axios.get('https://cvjson.now.sh/cv.json')
   // await for results
   const res = await fetchCvData()
 
