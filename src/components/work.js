7b3a510535fa098914d5600a9e400a56140a12d8
spences10 <spences10apps@gmail.com>
2018-10-11 22:12:07 +0100

    import workItem

--- src/components/work.js
+++ src/components/work.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
-import WorkItem from './WorkItem'
+import WorkItem from './workItem'
 
 // import { Dump } from '../util/helpers'
 
---
+++ src/components/workItem.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { format, isValid } from 'date-fns'
+import styled from 'styled-components'
+
+import {
+  ItemHeader as IH,
+  StyledDiv as SD,
+  StyledP as SP,
+  WeightAndColour as WC
+} from './shared'
+
+const WorkItemWrapper = styled(SD)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: auto;
+  grid-template-areas:
+    'comp comp pos pos date date'
+    'wh   wh   wh  wh  wh   wh  '
+    'w    w    w   w   w    w   '
+    'hh   hh   hh  hh  hh   hh  '
+    'h    h    h   h   h    h   ';
+  margin: 0.5rem;
+  padding: 0.5rem;
+`
+
+const Company = styled(WC)`
+  grid-area: comp;
+  margin-left: 0rem;
+  padding-left: 0rem;
+  padding-bottom: 0.125rem;
+  margin-bottom: 0.125rem;
+`
+
+const Position = styled(WC)`
+  grid-area: pos;
+  padding-bottom: 0.125rem;
+  margin-bottom: 0.125rem;
+`
+
+const Dates = styled(WC)`
+  grid-area: date;
+  padding-bottom: 0.125rem;
+  margin-bottom: 0.125rem;
+`
+
+const WorkItemHeader = styled(IH)`
+  grid-area: wh;
+`
+
+const WorkItemSummary = styled(SP)`
+  grid-area: w;
+  padding: 0.5rem 0rem 0.125rem 0rem;
+  margin: 0.5rem 0rem 0.125rem 0rem;
+`
+
+const HighlightsHeader = styled(IH)`
+  grid-area: hh;
+`
+
+const WorkItemHighlights = styled.ul`
+  grid-area: h;
+  padding: 0rem;
+  margin: 0rem;
+`
+
+const WorkItemLi = styled.li`
+  grid-area: h;
+  padding-top: 0.5rem;
+  margin-top: 0.5rem;
+`
+
+const WorkItem = props => {
+  const {
+    startDate: propsStartDate,
+    endDate: propsEndDate,
+    highlights,
+    position,
+    company,
+    summary
+  } = props.workItemData
+  const getWorkDates = () => {
+    const startDate = format(propsStartDate, 'MMM yyyy')
+    const endDate = () => {
+      if (isValid(propsEndDate)) {
+        return format(propsEndDate, 'MMM yyyy')
+      } else {
+        return 'Present'
+      }
+    }
+
+    return `${startDate} - ${endDate()}`
+  }
+
+  const getHighlights = highlights.map((item, index) => {
+    return <WorkItemLi key={index}>{item}</WorkItemLi>
+  })
+
+  return (
+    <WorkItemWrapper>
+      <Company>{company}</Company>
+      <Position>{position}</Position>
+      <Dates>{getWorkDates()}</Dates>
+      <WorkItemHeader>summary</WorkItemHeader>
+      <WorkItemSummary>{summary}</WorkItemSummary>
+      <HighlightsHeader>highlights</HighlightsHeader>
+      <WorkItemHighlights>{getHighlights}</WorkItemHighlights>
+    </WorkItemWrapper>
+  )
+}
+
+WorkItem.propTypes = {
+  workItemData: PropTypes.object
+}
+
+export default WorkItem
