ab1ff8f58b12d4d9c38f88489b100f9bae031396
spences10 <spences10apps@gmail.com>
2018-10-12 06:44:54 +0100

    add layout to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <div>
+  <Layout>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    <p>You just hit a route that does not exist... the sadness.</p>
+  </Layout>
 )
 
 export default NotFoundPage
