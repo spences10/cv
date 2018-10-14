9f947e01e4b87a3330fb5a38e989cd222626b26e
spences10 <spences10apps@gmail.com>
2018-10-14 15:56:16 +0100

    now location

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,9 @@
 const axios = require('axios')
 const createNodeHelpers = require('gatsby-node-helpers').default
 
-const fetchUrl =
-  'https://gist.githubusercontent.com/spences10/1920e5c8f91cd723fd25b90d41abaf8f/raw/a550b180121ad91968d4713c22b893df1b279855/cv.json'
 // const fetchUrl =
-//   'https://cvjson.now.sh/cv.json'
+//   'https://gist.githubusercontent.com/spences10/1920e5c8f91cd723fd25b90d41abaf8f/raw/a550b180121ad91968d4713c22b893df1b279855/cv.json'
+const fetchUrl = 'https://cvjson.now.sh/cv.json'
 
 const { createNodeFactory } = createNodeHelpers({
   typePrefix: 'cvData'
