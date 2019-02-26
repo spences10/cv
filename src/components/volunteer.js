261b8a802cfc782d252bd77892b74cf667826850
spences10 <spences10apps@gmail.com>
2019-02-26 08:07:19 +0000

    Remove StaticQuery

--- src/components/volunteer.js
+++ src/components/volunteer.js
@@ -1,6 +1,6 @@
-import { graphql, StaticQuery } from 'gatsby'
 import React from 'react'
 import styled from 'styled-components'
+import useCvData from './cvData'
 import { ItemWrapper as IW } from './shared'
 import WorkItem from './workItem'
 
@@ -8,8 +8,9 @@ const VolunteerWrapper = styled(IW)`
   grid-area: vo;
 `
 
-const Volunteer = ({ data }) => {
-  const { volunteer } = data.cvDataCv
+const Volunteer = () => {
+  const { volunteer } = useCvData()
+
   return (
     <React.Fragment>
       {volunteer[0].organization.length === 0 ? null : (
@@ -23,23 +24,4 @@ const Volunteer = ({ data }) => {
   )
 }
 
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query Volunteer {
-        cvDataCv {
-          volunteer {
-            organization
-            position
-            website
-            startDate
-            endDate
-            summary
-            highlights
-          }
-        }
-      }
-    `}
-    render={data => <Volunteer data={data} {...props} />}
-  />
-)
+export default Volunteer
