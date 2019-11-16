ca4aa6b9c451dfdabc401a2873c3442741229d61
spences10 <spences10apps@gmail.com>
2019-11-16 19:50:13 +0000

    config changes, remove favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   description: `Scott Spence, Father, husband 👨‍👩‍👧 Full stack web developer in the making 👨‍💻 Just In Time learner 👌 Byproduct of: coffee+excess carbs+lack of sleep. He/Him`,
   twitterUsername: `@ScottDevTweets`,
   facebookAppID: ``,
-  faviconPng: `./src/images/favicon.png`,
+  faviconPng: `./static/favicon.png`,
   backgroundColour: `#663399`, // this is for favicon and manifest
   themeColour: `#755f9f`, // this is for favicon and manifest
   nameContent: `Scott Spence - cv/resume`,
@@ -15,7 +15,7 @@ const siteMetadata = {
   developerUrl: `https.scottspence.me`,
   descriptionContent: `Scott Spence - web developer`,
   keywordsContent: `web developer, javascript, react, cv/resmue, information`,
-  imageLink: `https://scottspence.me/icons/icon-512x512.png`,
+  imageLink: `/favicon.png`,
   siteLanguage: `en-GB`
 }
 
@@ -34,44 +34,25 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: siteMetadata.faviconPng,
-        // WebApp Manifest Configuration
-        appName: siteMetadata.title,
-        appDescription: siteMetadata.descriptionContent,
-        developerName: siteMetadata.developerName,
-        developerURL: siteMetadata.developerUrl,
-        dir: `auto`,
-        lang: siteMetadata.siteLanguage,
-        background: siteMetadata.backgroundColour,
-        theme_color: siteMetadata.themeColour,
-        display: `standalone`,
-        orientation: `any`,
-        start_url: `/?homescreen=1`,
-        version: `1.0`,
-
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          opengraph: false,
-          twitter: false,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // add to netlify Build environment variables
         trackingId: process.env.GATSBY_GA_TRACKING_ID,
         anonymize: false
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cheat Sheets`,
+        short_name: `Cheat Sheets`,
+        start_url: `/`,
+        background_color: siteMetadata.backgroundColour,
+        theme_color: siteMetadata.themeColour,
+        display: `standalone`,
+        icon: siteMetadata.faviconPng
+      }
+    },
+    `gatsby-plugin-offline`
   ]
 }
