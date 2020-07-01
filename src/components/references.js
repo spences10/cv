2af5a0ccdecc54932a921060223db4376288c38d
spences10 <spences10apps@gmail.com>
2020-07-01 19:28:30 +0100

    components for MDX

---
+++ src/components/references.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const References = () => {
+  return (
+    <>
+      <h1>YO!</h1>
+    </>
+  )
+}
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,19 @@
+import { styled } from 'linaria/react'
 import React from 'react'
+import { useTheme } from '../theme/theme-provider'
+
+const StyledSkills = styled.article`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  background-color: ${({ theme }) => theme.colors.blue[500]};
+`
 
 export const Skills = ({ children }) => {
-  return <article style={{ color: 'red' }}>{children}</article>
+  const theme = useTheme()
+  return (
+    <>
+      <StyledSkills theme={theme}>{children}</StyledSkills>
+    </>
+  )
 }
