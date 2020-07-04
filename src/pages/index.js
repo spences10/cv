c586fcf3818516781f4a841f8f7bd5f6e5eb6718
spences10 <spences10apps@gmail.com>
2020-07-04 19:48:16 +0100

    change copy on slide

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { Slider } from '../components'
 import Long from '../copy/long'
 import Medium from '../copy/medium'
 import Short from '../copy/short'
 
+const Wrapper = styled.main``
+
+const ShortWrapper = styled.article`
+  display: ${({ visible }) => visible};
+`
+const MediumWrapper = styled.article`
+  display: ${({ visible }) => visible};
+`
+const LongWrapper = styled.article`
+  display: ${({ visible }) => visible};
+`
+
 export default () => {
   const [copySize, copySizeSet] = useState(0)
 
+  const copy = {
+    0: 'Short',
+    1: 'Medium',
+    2: 'Long',
+  }
+
   return (
-    <>
+    <Wrapper>
       <Slider copySizeSet={copySizeSet} />
-      <Short />
-      <Medium />
-      <Long />
-      <h1>{copySize}</h1>
-    </>
+      <h1>{copy[copySize]}</h1>
+
+      <ShortWrapper visible={copySize === '0' ? `block` : `none`}>
+        <Short />
+      </ShortWrapper>
+      <MediumWrapper visible={copySize === '1' ? `block` : `none`}>
+        <Medium />
+      </MediumWrapper>
+      <LongWrapper visible={copySize === '2' ? `block` : `none`}>
+        <Long />
+      </LongWrapper>
+    </Wrapper>
   )
 }
