cabcd30b27a98ed422f3b7e488b94ee97aebfc9a
spences10 <spences10apps@gmail.com>
2021-05-23 19:24:49 +0100

    add bas styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,28 @@
-<slot />
+<main>
+  <slot />
+</main>
+
+<style>
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(html) {
+    min-height: 100%;
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-size: 16px;
+  }
+
+  main {
+    position: relative;
+    max-width: 640px;
+    margin: 0 auto;
+  }
+</style>
