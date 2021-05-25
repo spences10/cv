cfabe3dee928ad7ca3185486ec67d1d5a5317984
spences10 <spences10apps@gmail.com>
2021-05-25 08:12:32 +0100

    add additional sections

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,9 @@
 <script>
   import Basics from '../copy/basics.md'
+  import Education from '../copy/education.md'
+  import Interests from '../copy/interests.md'
+  import References from '../copy/references.md'
+  import Skills from '../copy/skills.md'
   import Work from '../copy/work.md'
 </script>
 
@@ -15,4 +19,8 @@
 </svelte:head>
 
 <Basics />
+<Skills />
 <Work />
+<Interests />
+<Education />
+<References />
