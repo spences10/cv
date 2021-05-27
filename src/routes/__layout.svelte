ac671d831639f0fa71d94b9441e251bb1cdd7028
spences10 <spences10apps@gmail.com>
2021-05-27 21:27:36 +0100

    add markdown styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,9 @@
-<main class="container max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+<script>
+  import '../markdown.css'
+</script>
+
+<main
+  class="container max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 markdown"
+>
   <slot />
 </main>
