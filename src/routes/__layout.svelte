4a50162a63b79563e41a4c0137152f563876e38d
spences10 <spences10apps@gmail.com>
2021-05-28 09:07:21 +0100

    import markdown class globally

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,4 @@
-<script>
-  import '../markdown.css'
-</script>
-
-<main
-  class="container max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 markdown"
->
+<main class="container max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
   <slot />
 </main>
+
