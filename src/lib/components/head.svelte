1f5a89a9c0b8e82cad24d80a1bae93ce5860dc52
spences10 <spences10apps@gmail.com>
2021-11-07 17:08:35 +0000

    chore: :mag: add og cover image

--- src/lib/components/head.svelte
+++ src/lib/components/head.svelte
@@ -16,14 +16,20 @@
   <!-- Google / Search Engine Tags -->
   <meta itemprop="name" content={siteName} />
   <meta itemprop="description" content={description} />
-  <meta itemprop="image" content={`${siteUrl}/profile-pic.png`} />
+  <meta
+    itemprop="image"
+    content={`${siteUrl}/og-cover-image-scott-spence.jpg`}
+  />
 
   <!-- Facebook Meta Tags -->
   <meta property="og:url" content={siteUrl} />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={siteName} />
   <meta property="og:description" content={description} />
-  <meta property="og:image" content={`${siteUrl}/profile-pic.png`} />
+  <meta
+    property="og:image"
+    content={`${siteUrl}/og-cover-image-scott-spence.jpg`}
+  />
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image" />
@@ -31,7 +37,10 @@
   <meta property="twitter:url" content={siteUrl} />
   <meta name="twitter:title" content={siteName} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={`${siteUrl}/profile-pic.png`} />
+  <meta
+    name="twitter:image"
+    content={`${siteUrl}/og-cover-image-scott-spence.jpg`}
+  />
 
   <!-- Monetisation -->
   <meta name="monetization" content="$ilp.uphold.com/bzPBWkMBzLmN" />
