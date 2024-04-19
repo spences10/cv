6a9f0942307af5576adbc4dcea7c63a3fed7c4be
spences10 <spences10apps@gmail.com>
2024-04-19 18:11:15 +0100

    refactor: :recycle: use adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
