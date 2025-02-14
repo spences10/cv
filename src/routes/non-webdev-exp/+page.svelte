1166dc17968f3f6f3002a353c46c8bd4c6a101a2
spences10 <spences10apps@gmail.com>
2025-02-14 08:27:19 +0000

    refactor: update page store import from $app/stores to $app/state

--- src/routes/non-webdev-exp/+page.svelte
+++ src/routes/non-webdev-exp/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { authorName, siteName } from '$lib/config';
 	import { NonWebDevExp, References } from '$lib/copy';
 	import { Head, type SeoConfig } from 'svead';
@@ -7,7 +7,7 @@
 	const seo_config: SeoConfig = {
 		title: `Non Web Development Related Experience · ${siteName}`,
 		description: `Relevant past experience for ${authorName}`,
-		url: $page.url.href,
+		url: page.url.href,
 		site_name: siteName,
 		author_name: authorName,
 		payment_pointer: '$ilp.uphold.com/bzPBWkMBzLmN',
